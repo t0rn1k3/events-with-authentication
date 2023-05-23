8e6f6250015632057f30b09fb9129ee6de6b2e4e
t0rn1k3 <tornikeburjanadze2@gmail.com>
2023-05-24 01:51:25 +0400

    create server for project on localhost:3000

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const port = 3000;
+const api = require('./routes/api')
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use('/api', api)
+
+app.get('/', function(req, res){
+    res.send('hello')
+})
+
+app.listen(port, function(){
+    console.log('server works')
+})
