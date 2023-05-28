0acdd27ff5df2c9f328ef3c9dfbca703f9c8061d
t0rn1k3 <tornikeburjanadze2@gmail.com>
2023-05-29 02:34:34 +0400

    create form, input and button components, add styles on them

---
+++ src/app/shared/components/input/input.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import {FormControl} from "@angular/forms";
+
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss']
+})
+export class InputComponent {
+
+  @Input() type! : string
+  @Input() labelText! : string
+  @Input() control! : FormControl
+
+}
